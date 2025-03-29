3cf98e8e2c1b3599ea0e5c13a7b1a0279796003f
alejo-castellanos <alejandro.castellanos@grupor5.com>
2025-03-28 19:35:38 -0500

    feat: deploy github

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/fund-bridge-portal-15-frontend/",
   server: {
     host: "::",
     port: 8080,
